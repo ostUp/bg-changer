30c8bc0573f034edf601dcc51d48c8cd04c1013a
Ostap Blystsiv <ostapcr7@gmail.com>
2021-08-27 10:31:51 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+const hex = ['1', '2', '3', '4', '5', '6', '7', '8', 'A', 'B', 'C', 'D', 'E', 'F']
+
+btn.addEventListener('click', () => {
+	let hexColor = generateHex()
+	document.body.style.backgroundColor = hexColor
+	color.textContent = hexColor
+
+})
+
+function generateHex() {
+	let hexColor = '#'
+	for (let i = 0; i < 6; i++) {
+		hexColor += hex[getRandomNum()]
+	}
+	return hexColor;
+}
+
+function getRandomNum() {
+	return Math.floor(Math.random() * hex.length)
+}
